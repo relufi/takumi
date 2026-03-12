af6039c49005b9ba0990584d9dff70c4df647839
relufi <relufi@qq.com>
2026-03-12 23:13:11 +0800

    fix font feature and variation FromCss implementation

--- takumi/src/layout/style/properties/font_feature_settings.rs
+++ takumi/src/layout/style/properties/font_feature_settings.rs
@@ -1,5 +1,6 @@
-use cssparser::Parser;
+use cssparser::{BasicParseErrorKind, Parser, Token};
 use parley::FontFeature;
+use std::borrow::Cow;
 
 use crate::layout::style::{CssToken, FromCss, MakeComputed, ParseResult};
 
@@ -13,9 +14,45 @@ impl MakeComputed for FontFeatureSettings {}
 
 impl<'i> FromCss<'i> for FontFeatureSettings {
   fn from_css(input: &mut Parser<'i, '_>) -> ParseResult<'i, Self> {
-    Ok(Box::from_iter(FontFeature::parse_list(
-      input.current_line(),
-    )))
+    if input
+      .try_parse(|input| input.expect_ident_matching("normal"))
+      .is_ok()
+    {
+      return Ok(Box::new([]));
+    }
+    let list = input.parse_comma_separated(|input| {
+      let tag_name = input.expect_string()?;
+      if tag_name.len() != 4 || !tag_name.is_ascii() {
+        let err_token = tag_name.clone();
+        return Err(
+          input.new_error::<Cow<str>>(BasicParseErrorKind::UnexpectedToken(Token::QuotedString(
+            err_token,
+          ))),
+        );
+      }
+      let tag = swash::tag_from_str_lossy(tag_name);
+      let start_location = input.current_source_location();
+      let value = if input.is_exhausted() {
+        1f32
+      } else {
+        match input.next()? {
+          Token::Ident(st) if st.as_ref() == "on" => 1f32,
+          Token::Ident(st) if st.as_ref() == "off" => 0f32,
+          Token::Number { value, .. } => *value,
+          token => {
+            return Err(start_location.new_unexpected_token_error::<Cow<str>>(token.clone()));
+          }
+        }
+      };
+      if value > u16::MAX as f32 || value < 0f32 {
+        return Err(input.new_custom_error("Invalid font feature value"));
+      };
+      Ok(FontFeature {
+        tag,
+        value: value as u16,
+      })
+    })?;
+    Ok(list.into_boxed_slice())
   }
 
   fn from_str(source: &'i str) -> ParseResult<'i, Self> {
--- takumi/src/layout/style/properties/font_variation_settings.rs
+++ takumi/src/layout/style/properties/font_variation_settings.rs
@@ -1,7 +1,6 @@
-use cssparser::Parser;
-use parley::FontVariation;
-
 use crate::layout::style::{CssToken, FromCss, MakeComputed, ParseResult};
+use cssparser::{BasicParseErrorKind, Parser};
+use parley::FontVariation;
 
 /// Controls variable font axis values via CSS font-variation-settings property.
 ///
@@ -13,9 +12,25 @@ impl MakeComputed for FontVariationSettings {}
 
 impl<'i> FromCss<'i> for FontVariationSettings {
   fn from_css(input: &mut Parser<'i, '_>) -> ParseResult<'i, Self> {
-    Ok(Box::from_iter(FontVariation::parse_list(
-      input.current_line(),
-    )))
+    if input
+      .try_parse(|input| input.expect_ident_matching("normal"))
+      .is_ok()
+    {
+      return Ok(Box::new([]));
+    }
+    let list = input.parse_comma_separated(|input| {
+      let tag_name = input.expect_string()?;
+      if tag_name.len() != 4 || !tag_name.is_ascii() {
+        let err_token = tag_name.clone();
+        return Err(input.new_error::<std::borrow::Cow<str>>(
+          BasicParseErrorKind::UnexpectedToken(cssparser::Token::QuotedString(err_token)),
+        ));
+      }
+      let tag = swash::tag_from_str_lossy(tag_name);
+      let value = input.expect_number()?;
+      Ok(FontVariation { tag, value })
+    })?;
+    Ok(list.into_boxed_slice())
   }
 
   fn from_str(source: &'i str) -> ParseResult<'i, Self> {
